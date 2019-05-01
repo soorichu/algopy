6728075bc62db7b1145de5ff495b7052eb17991d
unknown <soorichu@gmail.com>
2019-05-01 11:08:44 +0900

    fix

--- other/pylife2.py
+++ other/pylife2.py
@@ -1,12 +1,10 @@
 def printA(a, n):
 	for i in range(1, n+1):
 		for j in range(1, n+1):
-			if a[i][j]==0: print(" ", end="")
-			else: print("■", end="")
+			if a[i][j]==0: print(" .", end="")
+			else: print(" ■", end="")
 		print()
 
-di = (0, 1, 1, 1, 0, -1, -1, -1)
-dj = (1, 1, 0, -1, -1, -1, 0, 1)
 now = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -26,14 +24,14 @@ now = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 def life2(a, n):
   print(">> Origin")
   printA(a, len(a)-2)
-  next =[[0]*(len(a)+2) for i in range(len(a)+2)]
-  for _ in range(n):
+  next =[[0]*(len(a)) for i in range(len(a))]
+  for k in range(n):
     for i in range(1, len(a)-1):
       for j in range(1, len(a)-1):
         around = sum(a[i-1][j-1:j+2])+sum(a[i][j-1:j+2])+sum(a[i+1][j-1:j+2])
-        if (around == 3) or (around -a[i][j] == 3): next[i][j] = 1
+        if (around == 3) or (around-a[i][j] == 3): next[i][j] = 1
         else: next[i][j] = 0
-    print(">> After %d" %(_+1))
+    print(">> After %d" %(k+1))
     printA(next, len(next)-2)
     a = next
 
